bbbb1c04dafbe591ed577b32c34156d873fed970
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2020-11-14 12:39:04 +0530

    ACC/HOM new files

---
+++ ACC_SECollectionsPage.php
@@ -0,0 +1,98 @@
+<html>
+	<head>
+        
+		<title>ACC_SECollectionsPage</title>
+		<link rel="stylesheet" type="text/css" href="../css/nav.css">
+        <link rel="stylesheet" type="text/css" href="../css/dealerCategoryPage.css">
+        <link rel="stylesheet" type="text/css" href="../css/ACCSECollectionsPage.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+	 		body {
+       			background-color: #000;
+  				background-image: url("../img/background.jpg");
+  				background-size: cover;
+  				background-position: top center;
+	 		}
+		</style>
+	</head>
+	<body>
+    	<div class="container">
+            <div class="header">
+                <?php include '../html/ACC_header.html';?>
+            </div>
+            <h3>Payments collected by SE</h3>
+            <div class="clearfix">
+                <table>
+                    <thead>
+                        <tr>
+                            <th style="width:20%">Invoice NO.</th>
+                            <th style="width:20%">Dealer ID</th>
+                            <th style="width:20%">Amount</th>
+                            <th style="width:20%">Date</th>
+                            <th style="width:20%">Confirmation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxx</td>
+                            <td>xxxx.xx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>
+                                <button><i class="fa fa-check-circle" aria-hidden="true"></i> Confirm</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxx</td>
+                            <td>xxxx.xx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>
+                                <button><i class="fa fa-check-circle" aria-hidden="true"></i> Confirm</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxx</td>
+                            <td>xxxx.xx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>
+                                <button><i class="fa fa-check-circle" aria-hidden="true"></i> Confirm</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxx</td>
+                            <td>xxxx.xx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>
+                                <button><i class="fa fa-check-circle" aria-hidden="true"></i> Confirm</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxx</td>
+                            <td>xxxx.xx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>
+                                <button><i class="fa fa-check-circle" aria-hidden="true"></i> Confirm</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxx</td>
+                            <td>xxxx.xx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>
+                                <button><i class="fa fa-check-circle" aria-hidden="true"></i> Confirm</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="footercontent">
+                <?php include '../html/footer.html';?>
+            </div>
+        </div>
+	</body>
+</html>
---
+++ HOM_dealerDetailsPage.php
@@ -0,0 +1,116 @@
+<html>
+	<head>
+        
+		<title>HOM_DealerDetailsPage</title>
+		<link rel="stylesheet" type="text/css" href="../css/nav.css">
+        <link rel="stylesheet" type="text/css" href="../css/dealerCategoryPage.css">
+        <link rel="stylesheet" type="text/css" href="../css/HOMDealerDetailsPage.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+	 		body {
+       			background-color: #000;
+  				background-image: url("../img/background.jpg");
+  				background-size: cover;
+  				background-position: top center;
+	 		}
+		</style>
+	</head>
+	<body>
+    	<div class="container">
+            <div class="header">
+                <?php include '../html/HOM_header.html';?>
+            </div>
+            <h3>Dealer Details</h3>
+            <div class="clearfix">
+                <table>
+                    <thead>
+                        <tr>
+                            <th style="width:15%">Dealer ID</th>
+                            <th style="width:20%">Name</th>
+                            <th style="width:15%">Telephone NO.</th>
+                            <th style="width:20%">Due Orders</th>
+                            <th style="width:20%">Due Amount</th>
+                            <th style="width:10%">Due Order Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                        <tr>
+                            <td>xxx</td>
+                            <td>xxxx xxxx xxxxxxxxxx</td>
+                            <td>xxxxxxxxxx</td>
+                            <td>xxx<br/>xxx</td>
+                            <td>xxxx.xx<br/>xxxx.xx</td>
+                            <td>xx/xx/xxxx<br/>xx/xx/xxxx</td>   
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="footercontent">
+                <?php include '../html/footer.html';?>
+            </div>
+        </div>
+	</body>
+</html>
