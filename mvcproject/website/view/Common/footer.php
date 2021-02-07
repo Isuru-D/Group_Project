5b9445c0f015464266427bb76a53386587e51435
Isuru-Diyawinna <56600654+Isuru-Diyawinna@users.noreply.github.com>
2021-02-07 11:45:52 +0530

    Delete mvcproject directory

--- mvcproject/website/view/Common/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="logo">
-    <img src="<?php echo URL; ?>public/img/logo.png" height =103px width=320px>
-</div>
-<div class="footer">
-    <h3>copyright &copy; BrandlinkOverseas.lk</h3>
-    <a href="https://www.facebook.com"><img src="<?php echo URL; ?>public/img/fb.png" width=40px height=40px></a>
-    <a href="https://www.instagram.com/?hl=en"><img src="<?php echo URL; ?>public/img/insta.png" width=60px height=40px></a>
-    <a href="https://twitter.com/"><img src="<?php echo URL; ?>public/img/twitter.png" width=40px height=40px></a>
-</div>
